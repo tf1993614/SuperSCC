a00fb39028e7722ddc5913180bb034317d8e5137
tf1993614 <61369770+tf1993614@users.noreply.github.com>
2025-02-09 20:26:47 +0800

    Add files via upload

---
+++ SuperSCC/__init__.py
@@ -0,0 +1,37 @@
+import re
+import os
+import sys
+import pickle
+import time
+import random
+import warnings
+from datetime import datetime
+import logging
+from functools import reduce
+from multiprocessing import Process
+from collections import Counter
+import copy
+
+from rpy2.robjects.packages import importr
+from dcor import distance_correlation
+import scanpy as sc
+from scanpy import AnnData
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from scipy.spatial.distance import pdist, squareform
+from scipy.stats import rankdata, gmean
+from sklearn.metrics import accuracy_score, cohen_kappa_score, matthews_corrcoef, balanced_accuracy_score, make_scorer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import RFECV,VarianceThreshold,SelectKBest,chi2, SelectFromModel, f_classif, mutual_info_classif
+from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler
+import matplotlib.pyplot as plt
+
+from .SuperSCC import *
+from ._version import __version__
