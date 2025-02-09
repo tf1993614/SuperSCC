866c5512c4f00683d57086c34c707e8868c545c2
tf1993614 <61369770+tf1993614@users.noreply.github.com>
2025-02-09 20:42:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 version = {}
-with open("Garfield/_version.py") as fp:
+with open("SuperSCC/_version.py") as fp:
     exec(fp.read(), version)
 
 setup(
