07932ed72902397569908cb275965b90ea868974
tf1993614 <61369770+tf1993614@users.noreply.github.com>
2025-02-11 13:17:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
        "leidenalg==0.10.2", 
        "plotly==5.22.0", 
        "rpy2==3.5.17", 
-       "langchain",
-       "langchain-community"
+       "langchain==0.3.18",
+       "langchain-community==0.3.17"
     ]
 )
