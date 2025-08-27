53a51a9e71c6a9ac35ad906f12a6e3ccc3a1dcbe
Feng Tang <763615402@qq.com>
2025-08-27 13:45:17 +0800

    update init file

--- SuperSCC/__init__.py
+++ SuperSCC/__init__.py
@@ -1,41 +1,12 @@
-import re
-import os
-import sys
-import pickle
-import time
-import random
-import warnings
-from datetime import datetime
-import logging
-from functools import reduce
-from multiprocessing import Process
-from collections import Counter
-import copy
-
-from langchain_community.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
-from rpy2.robjects.packages import importr
-from dcor import distance_correlation
-import scanpy as sc
-from scanpy import AnnData
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-from scipy.spatial.distance import pdist, squareform
-from scipy.stats import rankdata, gmean
-from sklearn.metrics import accuracy_score, cohen_kappa_score, matthews_corrcoef, balanced_accuracy_score, make_scorer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC, LinearSVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.decomposition import PCA
-from sklearn.feature_selection import RFECV,VarianceThreshold,SelectKBest,chi2, SelectFromModel, f_classif, mutual_info_classif
-from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler
-import matplotlib.pyplot as plt
-import magic
-
-from .SuperSCC import *
+from . import feature_selection, clustering, gene_module, label_transfer, utils
 from ._version import __version__
+
+
+__all__ = [
+    "feature_selection",
+    "clustering",
+    "gene_module",
+    "label_transfer",
+    "utils"
+]
+
